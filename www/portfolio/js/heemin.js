34ff8f89bf47f9429d64a47f12dabcd3609d87a3
benecia1221 <gitgmlalsdlfdl12>
2021-06-30 18:13:12 +0900

    제작노트 추가완료 + 슬릭슬라이더 추가

--- www/portfolio/js/heemin.js
+++ www/portfolio/js/heemin.js
@@ -19,7 +19,19 @@ $(document).ready(function(){
              $("#sec2").removeClass("on");
         }
         lastScroll = scroll;
- });
+    });
+
+    $(window).scroll(function(event){
+         var scroll = $(this).scrollTop();
+         if (scroll > 2700){
+              $("#sec3").addClass("on");
+         }
+         else {
+              $("#sec3").removeClass("on");
+         }
+         lastScroll = scroll;
+     });
+
    $(".bottom_btn a, .menu a, a[href='#pageTop']").click(function(e){
      e.preventDefault();//e:임의의 a태그 변수지정
      var targetPos = $($(this).attr('href')).offset().top
@@ -40,8 +52,54 @@ $(document).ready(function(){
     $(".planning_noteImg.note1").fadeIn();
     return false;
   });
+  $(".planning_back .noteLink2").click(function(){
+    $(".planning_note").fadeIn();
+    $(".planning_noteImg.note2").fadeIn();
+    return false;
+  });
+  $(".planning_back .noteLink3").click(function(){
+    $(".planning_note").fadeIn();
+    $(".planning_noteImg.note3").fadeIn();
+    return false;
+  });
+  $(".planning_back .noteLink4").click(function(){
+    $(".planning_note").fadeIn();
+    $(".planning_noteImg.note4").fadeIn();
+    return false;
+  });
+  $(".planning_back .noteLink2").click(function(){
+    $(".planning_note").fadeIn();
+    $(".planning_noteImg.note2").fadeIn();
+    return false;
+  });
+  $(".planning_back .noteLink5").click(function(){
+    $(".planning_note").fadeIn();
+    $(".planning_noteImg.note5").fadeIn();
+    return false;
+  });
+  $(".planning_back .noteLink6").click(function(){
+    $(".planning_note").fadeIn();
+    $(".planning_noteImg.note6").fadeIn();
+    return false;
+  });
+
+
+
 
   $(".planning_note .close_button").click(function(){
     $(".planning_note").fadeOut();
+    $(".planning_noteImg").fadeOut();
   });
+
+  $("#slider").slick({
+    slide:'div',
+    infinite:true,
+    slidesToShow : 4, 
+    slidesToScroll:1,
+    speed:100,
+    arrows:true,
+    autoplay : true,
+  })
+
+
 });
